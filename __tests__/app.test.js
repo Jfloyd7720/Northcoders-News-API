9398eefcf82cd130b12e0d93fb57625b2d4211fb
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-27 14:24:07 +0000

    add relevant imports and run seed in test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,13 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const request = require("supertest");
+const data = require("../db/data/test-data");
+const seed = require("../db/seeds/seed");
+const app = require("../app");
 
 /* Set up your beforeEach & afterAll functions here */
+beforeEach(() => {
+  return seed(data);
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
