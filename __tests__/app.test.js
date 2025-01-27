62e9a42aea23e242589c02aec76299ab810e77fd
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-27 19:03:23 +0000

    add a test for the /api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,16 @@ const request = require("supertest");
 const data = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
 const app = require("../app");
+const db = require("../db/connection");
 
 /* Set up your beforeEach & afterAll functions here */
 beforeEach(() => {
   return seed(data);
 });
 
+afterAll(() => {
+  db.end();
+});
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
@@ -16,6 +20,20 @@ describe("GET /api", () => {
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+describe("GET /api/topics", () => {
+  test("200: Responds with all the data from the topics tables", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual([
+          { slug: "mitch", description: "The man, the Mitch, the legend" },
+          { slug: "cats", description: "Not dogs" },
+          { slug: "paper", description: "what books are made of" },
+        ]);
       });
   });
 });
