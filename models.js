0e84adfd8c37f6b4d5ab57974fa9f0dbdd3e30af
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-28 16:26:21 +0000

    add model function to fetch data by article id

--- models.js
+++ models.js
@@ -6,4 +6,13 @@ const fetchData = () => {
   });
 };
 
-module.exports = { fetchData };
+const fetchArticlesDataById = (param) => {
+  const query = `SELECT * FROM articles WHERE article_id = $1 `;
+  return db.query(query, [param.article_id]).then((res) => {
+    if (res.rows.length > 0) return res.rows;
+    else {
+      return null;
+    }
+  });
+};
+module.exports = { fetchData, fetchArticlesDataById };
