57bcf2c92b81994063e1d375f6aade64286fa928
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-28 23:17:26 +0000

    add model function for articles endpoint

--- models.js
+++ models.js
@@ -1,9 +1,37 @@
 const db = require("./db/connection");
 
-const fetchData = () => {
+const fetchTopicsData = () => {
   return db.query("SELECT * FROM topics").then((res) => {
     return res.rows;
   });
+};
+
+const fetchArticleData = () => {
+  return db
+    .query(
+      `SELECT 
+  articles.author, 
+  articles.title, 
+  articles.article_id, 
+  articles.topic, 
+  articles.created_at, 
+  articles.votes, 
+  articles.article_img_url,
+  COUNT(comments.article_id) AS comment_count 
+FROM articles LEFT JOIN comments ON articles.article_id=comments.article_id
+GROUP BY   articles.author, 
+articles.title, 
+articles.article_id, 
+articles.topic, 
+articles.created_at, 
+articles.votes, 
+articles.article_img_url
+ORDER BY articles.created_at DESC`
+    )
+    .then((res) => {
+      console.log(res.rows);
+      return res.rows;
+    });
 };
 
 const fetchArticlesDataById = (param) => {
@@ -15,4 +43,9 @@ const fetchArticlesDataById = (param) => {
     }
   });
 };
-module.exports = { fetchData, fetchArticlesDataById };
+
+module.exports = {
+  fetchTopicsData,
+  fetchArticlesDataById,
+  fetchArticleData,
+};
