51f0c3cef1fe16ecef904918fd102be03ed34b63
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-28 16:24:05 +0000

    add article by id endpoint

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const endpoints = require("./endpoints.json");
-const { getApiInfo, getTopicsData } = require("./controllers");
+const { getApiInfo, getTopicsData, getArticlesByID } = require("./controllers");
 
 app.get("/api", getApiInfo);
 
 app.get("/api/topics", getTopicsData);
+
+app.get("/api/articles/:article_id", getArticlesByID);
 
 app.all("*", (req, res) => {
   res.status(404).send({ error: "endpoint not fpund" });
