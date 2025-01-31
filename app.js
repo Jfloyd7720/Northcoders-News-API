434971510b01aa11b3f05a60efac76a870e269cd
Jfloyd7720 <floyd.latc@gmail.com>
2025-01-31 10:25:31 +0000

    add GET endpoint for api-users

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const {
   postCommentsDataByArticleID,
   patchArticleByArticleID,
   deleteCommentsByCommentID,
+  getUsers,
 } = require("./controllers");
 
 app.use(express.json());
@@ -28,6 +29,8 @@ app.post("/api/articles/:article_id/comments", postCommentsDataByArticleID);
 app.patch("/api/articles/:article_id", patchArticleByArticleID);
 
 app.delete("/api/comments/:comment_id", deleteCommentsByCommentID);
+
+app.get("/api/users", getUsers);
 
 app.all("*", (req, res) => {
   res.status(404).send({ error: "endpoint not fpund" });
